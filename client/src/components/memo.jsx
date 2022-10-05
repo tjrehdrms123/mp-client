b1530bc402cb47caae65b2b76212e5ab9e5c6880
tjrehdrms123 <wwe5455@naver.com>
2022-10-05 11:09:28 +0900

    [ Add ] kakao-map-sdk, map 버그 수정

--- client/src/components/memo.jsx
+++ client/src/components/memo.jsx
@@ -1,15 +1,49 @@
 import React from 'react'
-import { useEffect } from 'react'
-import settingKakaoMap from '../utils/map';
+import { useState } from 'react';
+import { useEffect } from 'react';
+import { Map, MapMarker, MarkerClusterer } from 'react-kakao-maps-sdk';
+import { pageGetAllAPI } from "../api/api";
+import { setLocalStorageAuthorizationToken } from "../utils/interceptor";
 
 function Memo() {
-  useEffect(()=> {
-    settingKakaoMap();
+  const [mapData, setMapData] = useState();
+  const pageGetAllData = async () => {
+    setLocalStorageAuthorizationToken(); // axios전 token 인터셉터 셋팅
+    const getPageData = await pageGetAllAPI();
+    const { data, status } = getPageData;
+    setMapData(data);
+  }
+  useEffect(()=>{
+    pageGetAllData();
   },[]);
   return (
     <>
     <div className="background">
-        <div id="map"></div>
+    <Map
+        center={{ lat: 37.566812940227386, lng: 126.9786522620371 }}
+        style={{ width: "80%", height: "360px", display: "block", margin: "0 auto"}}
+        level={9}
+    >
+        <MarkerClusterer
+            averageCenter={true}
+            minLevel={5}
+        >
+        </MarkerClusterer>
+        {
+            mapData&&mapData.map((items) => {
+                return <MapMarker
+                position={{
+                    lat: items.lat,
+                    lng: items.lng
+                }}
+                image={{
+                    src:items.markerimg.url,
+                    size:{width:60,height:60}
+                }}
+              />
+            })
+        }
+    </Map>
         <div className="container bootstrap snippets bootdeys">
             <div className="row">
                 <div className="col-md-4 col-sm-6 content-card">
