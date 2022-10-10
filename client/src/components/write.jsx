ad17c3806d563d17eb5a645045dc5b2440301622
tjrehdrms123 <wwe5455@naver.com>
2022-10-11 00:13:21 +0900

    [ Bug ] 조건문에러 수정

--- client/src/components/write.jsx
+++ client/src/components/write.jsx
@@ -90,24 +90,16 @@ function Writer() {
         map: map,
         position: new kakao.maps.LatLng(lat, lng),
       });
-      geocoder.addressSearch(address, function (result, status) {
-        // 정상적으로 검색이 완료됐으면
-        if (status === kakao.maps.services.Status.OK) {
-          var coords = new kakao.maps.LatLng(result[0].y, result[0].x);
-
-          // 인포윈도우로 장소에 대한 설명을 표시합니다
-          var infowindow = new kakao.maps.InfoWindow({
-            content:
-              '<div style="width:150px;text-align:center;padding:6px 0;">' +
-              address +
-              "</div>",
-          });
-          infowindow.open(map, marker);
-
-          // 지도의 중심을 결과값으로 받은 위치로 이동시킵니다
-          map.setCenter(coords);
-        }
-      });
+      if (address) {
+        geocoder.addressSearch(address, function (result, status) {
+          // 정상적으로 검색이 완료됐으면
+          if (status === kakao.maps.services.Status.OK) {
+            var coords = new kakao.maps.LatLng(result[0].y, result[0].x);
+            // 지도의 중심을 결과값으로 받은 위치로 이동시킵니다
+            map.setCenter(coords);
+          }
+        });
+      }
       kakao.maps.event.addListener(map, "center_changed", function () {
         var latlng = map.getCenter();
         setLat(latlng.getLat());
